199aa030bec8f7b0287cc9998be6a969314b97d8
Alecu Marco <136191501+MarcoPolo236@users.noreply.github.com>
2024-10-19 22:39:55 +0300

    task 8 and 9 finished

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();     // metoda care previne reincarcarea paginii
+
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (!email || !password) {
+    alert('Toate câmpurile trebuie completate!');
+    return;
+  }
+  const formData = {
+    email,
+    password
+  };
+
+    
+    
+  console.log(formData); 
+  form.reset(); 
+});
 
