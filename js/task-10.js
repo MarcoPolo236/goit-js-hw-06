dbcb646a2fb429ff6c6454437132606ab990d547
Alecu Marco <136191501+MarcoPolo236@users.noreply.github.com>
2024-10-19 22:47:40 +0300

    optional task done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = {
+  input: document.querySelector('#controls input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxesContainer: document.getElementById('boxes')
+};
+
+function createBoxes(amount) {
+  const boxes = [];
+  let size = 30; 
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = '5px';
+    boxes.push(box);
+    size += 10; 
+  }
+  controls.boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  controls.boxesContainer.innerHTML = '';
+}
+
+controls.createBtn.addEventListener('click', () => {
+  const amount = parseInt(controls.input.value, 10);
+  createBoxes(amount);
+});
+
+controls.destroyBtn.addEventListener('click', destroyBoxes);
